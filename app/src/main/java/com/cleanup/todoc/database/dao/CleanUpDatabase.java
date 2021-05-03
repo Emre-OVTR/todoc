bb3f152ea1c84ece5aa69db87749eab3cb8fee98
Emre Turhal <you@example.com>
2021-05-03 02:51:49 +0200

    CleanUpDatabase

---
+++ app/src/main/java/com/cleanup/todoc/database/dao/CleanUpDatabase.java
@@ -0,0 +1,59 @@
+package com.cleanup.todoc.database.dao;
+
+import android.arch.persistence.db.SupportSQLiteDatabase;
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.ContentValues;
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.cleanup.todoc.model.Project;
+import com.cleanup.todoc.model.Task;
+
+@Database(entities = {Project.class, Task.class}, version = 1, exportSchema = false)
+public abstract class CleanUpDatabase extends RoomDatabase {
+
+    public static volatile CleanUpDatabase INSTANCE;
+
+    public abstract TaskDao taskDao();
+
+    public static CleanUpDatabase getInstance(Context context) {
+
+        if (INSTANCE == null){
+
+            synchronized (CleanUpDatabase.class) {
+                if (INSTANCE== null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                                                    CleanUpDatabase.class, "MyDatebase.db")
+                                                    .addCallback(prepopulateDatabase())
+                                                    .build();
+                }
+            }
+
+        }
+        return INSTANCE;
+    }
+
+
+    private static Callback prepopulateDatabase(){
+        return new Callback() {
+
+            @Override
+            public void onCreate(@NonNull SupportSQLiteDatabase db) {
+                super.onCreate(db);
+
+                ContentValues contentValues = new ContentValues();
+                contentValues.put("id", 1);
+                contentValues.put("projectId", 1);
+                contentValues.put("name", "Test");
+                contentValues.put("creationTimestamp", 43);
+
+
+                db.insert("User", OnConflictStrategy.IGNORE, contentValues);
+            }
+        };
+    }
+
+}
